37fb27ce4feddf1ecf313998e9aa326d9ddde2fc
Your Name <chanrhamni>
2023-01-21 15:12:15 +0530

    update and upload hello-travel

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+export const travelData = [
+    {
+        name: "Kashmir Houseboat Tour",
+        package: "Package Starting:Srinagar | Gulmarg |",
+        price: 10999,
+    },
+    {
+        name: "Honeymoon In Kashmir",
+        package: "Package Starting:Srinagar | Gulmarg |",
+        price: 8999,
+    }
+]
