539d8cd2453bce2e883000c930e1379c4e8bb29b
Your Name <chanrhamni>
2023-01-21 15:22:02 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import Home from './components/home/Home'
+import Header from './components/header/Header'
+
 
 function App() {
 
 
   return (
     <div className="App">
-      <Home />
+      <Header />
     </div>
   )
 }
