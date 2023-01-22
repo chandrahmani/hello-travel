b9f2f4365a25780be14fa3e9f01b4870ebea5e66
Your Name <chanrhamni>
2023-01-22 15:40:56 +0530

    update service info

---
+++ src/components/service/Servic.jsx
@@ -0,0 +1,63 @@
+import './Service.css'
+
+const Servic = () => {
+    return (
+        <section class="service-section">
+            <div class="container">
+                <div class="heading-dtl">
+                    <h1>Complete Traveling Services</h1>
+                    <p className='color'>Plan as you want to. Book your desired hotels & tour packages. Book only what you want. No rigid itineraries. Nowhere else would you find a more flexible plan. </p>
+                </div>
+
+                <div>
+                    <div className='servic-wrp'>
+                        <div className='box'>
+                            <div className="img-wrp">
+                                <img src="https://kashmirtravelagent.com/images/Private_Guest_House.png" />
+                            </div>
+                            <div className="title-wrp">
+                                <p >title h5 text-darkHotels & Houseboats
+                                    sCheck out the best hotels & Houseboats in Kashmir and enjoy your vacation with your closed ones</p>
+                            </div>
+                        </div>
+
+                        <div className='box'>
+                            <div className="img-wrp">
+                                <img src="https://kashmirtravelagent.com/images/Sunset_on_Beach.png" />
+                            </div>
+                            <div className="title-wrp">
+                                <p >title h5 text-darkHotels & Houseboats
+                                    sCheck out the best hotels & Houseboats in Kashmir and enjoy your vacation with your closed ones</p>
+                            </div>
+                        </div>
+
+                        <div className='box'>
+                            <div className="img-wrp">
+                                <img src="https://kashmirtravelagent.com/images/Restaurant.png" />
+                            </div>
+                            <div className="title-wrp">
+                                <p >title h5 text-darkHotels & Houseboats
+                                    sCheck out the best hotels & Houseboats in Kashmir and enjoy your vacation with your closed ones</p>
+                            </div>
+                        </div>
+
+                        <div className='box'>
+                            <div className="img-wrp">
+                                <img src="https://kashmirtravelagent.com/images/Private_Guest_House.png" />
+                            </div>
+                            <div className="title-wrp">
+                                <p >title h5 text-darkHotels & Houseboats
+                                    sCheck out the best hotels & Houseboats in Kashmir and enjoy your vacation with your closed ones</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div >
+        </section >
+
+    )
+}
+
+export default Servic;
