9f43cd2a88cccc9cafb09048f98b243c1e45d4eb
Your Name <chanrhamni>
2023-01-22 12:42:00 +0530

    update img

--- src/components/travelDtl/TravelDtl.jsx
+++ src/components/travelDtl/TravelDtl.jsx
@@ -9,22 +9,70 @@ const TravelDtl = () => {
             <section className="cards-info">
                 <div class="grid-container">
                     <div class="grid-item"> <span className="offer">70% off </span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
+                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
+
+                    <div class="grid-item"> <span className="offer">50% off</span>
+                        <img src="https://kashmirtravelagent.com/images/travel/2.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
+
+                    <div class="grid-item"> <span className="offer">80% off</span>
+                        <img src="https://kashmirtravelagent.com/images/travel/9.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
+
+                    <div class="grid-item"> <span className="offer">40% off</span>
+                        <img src="https://kashmirtravelagent.com/images/travel/5.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
 
                     <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
+                        <img src="https://kashmirtravelagent.com/images/travel/4.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
 
                     <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
-
-                    <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
-
-                    <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
-
-                    <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span></div>
+                        <img src="https://kashmirtravelagent.com/images/travel/5.jpg" /><span className="tag">4N/5D</span>
+                        <div className="info-wrappers">
+                            <h3>Kashmir Houseboat Tour</h3>
+                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
+                            <h5>Sonmarg | Pahalgam</h5>
+                            <p>RS. 8,999/-</p>
+                            <button className="btns">Get Quote</button>
+                        </div>
+                    </div>
                 </div>
 
             </section>
