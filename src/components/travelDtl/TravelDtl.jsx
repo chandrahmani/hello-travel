220563ac179d507917fa9273ccd7f7e91eba1016
Your Name <chanrhamni>
2023-01-25 15:25:18 +0530

    update file add json data

--- src/components/travelDtl/TravelDtl.jsx
+++ src/components/travelDtl/TravelDtl.jsx
@@ -1,88 +1,55 @@
 import { useEffect, useState } from "react"
-
 import Servic from "../service/Servic"
 import './Traval.css'
+import axios from "axios";
+
+
+// const pth = "../../data/index.json"
 
 const TravelDtl = () => {
     const [data, setData] = useState([])
 
-    const travelData = () => {
+    //  console.log(data)
 
+    const travelData = async () => {
+        try {
+            const { data } = await axios.get('data.json')
+            console.log(data)
+            setData(data)
+        } catch (error) {
+            console.log(error)
+
+        }
     }
 
     useEffect(() => {
-
+        travelData()
     }, [])
+
+
     return (
         <>
             <section className="cards-info">
-                <div className="grid-container">
-                    <div className="grid-item"> <span className="offer">70% off </span>
-                        <img src="https://kashmirtravelagent.com/images/travel/1.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
+                {data.map((item, index) =>
+                    <div className="grid-container" key={index}>
+                        <div className="grid-item">
+                            <div className="info-wrappers">
+                                <span className="offer">{item.offer}</span>
+                                <img src={item.url} />
+                                {/* <span className="offer">4N/5D</span> */}
+                                <h3>{item.name}</h3>
+                                <h4>{item.package}</h4>
+                                <h5>Rs-/ {item.price}</h5>
+                                <p>
+                                    {item.place}
+                                </p>
+                                <button className="btns">Get Quote</button>
+                            </div>
                         </div>
+
                     </div>
 
-                    <div className="grid-item"> <span className="offer">50% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/2.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
-                        </div>
-                    </div>
-
-                    <div class="grid-item"> <span className="offer">80% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/9.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
-                        </div>
-                    </div>
-
-                    <div class="grid-item"> <span className="offer">40% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/5.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
-                        </div>
-                    </div>
-
-                    <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/4.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
-                        </div>
-                    </div>
-
-                    <div class="grid-item"> <span className="offer">70% off</span>
-                        <img src="https://kashmirtravelagent.com/images/travel/5.jpg" /><span className="tag">4N/5D</span>
-                        <div className="info-wrappers">
-                            <h3>Kashmir Houseboat Tour</h3>
-                            <h4>Package Starting:Srinagar | Gulmarg |</h4>
-                            <h5>Sonmarg | Pahalgam</h5>
-                            <p>RS. 8,999/-</p>
-                            <button className="btns">Get Quote</button>
-                        </div>
-                    </div>
-                </div>
+                )}
 
             </section>
 
