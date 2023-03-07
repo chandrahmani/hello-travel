cb194ed03b8fea40de9a9ab5469687844fee0329
Your Name <chanrhamni>
2023-03-07 10:34:20 +0530

    update file or css

--- src/components/service/Servic.jsx
+++ src/components/service/Servic.jsx
@@ -1,4 +1,5 @@
 import Adventor from '../adventur/Adventur';
+import { Container } from '../header/Styled';
 import './Service.css'
 
 const Servic = () => {
@@ -6,7 +7,7 @@ const Servic = () => {
         <>
             <section >
                 <div className="service-section">
-                    <div className="container">
+                    <Container>
                         <div className="heading-dtl">
                             <h1>Complete Traveling Services</h1>
                             <p className='color'>Plan as you want to. Book your desired hotels & tour packages. Book only what you want. No rigid itineraries. Nowhere else would you find a more flexible plan. </p>
@@ -57,26 +58,31 @@ const Servic = () => {
                             </div>
 
                         </div>
-                    </div >
-                </div>
+
+                    </Container>
+                </div >
             </section >
 
             <section className='section-tow'>
                 <div className='section-conatiner' >
-                    <p>
-                        <span className='txt'>About Kashmir Tourism</span>< br />
-                        "Paradise on the Earth" the state is world famous for its scenic splendour, snow-capped mountains, plentiful wildlife, hospitable people and local handicraft.
-                        With apple orchards, saffron fields, glittering lakes & breathtaking meadows, Kashmir, in the union territory of Jammu & Kashmir, draws the attention of tourists from across the globe. With tranquil settings, magnificent views, and pristine nature, Kashmir is a perfect holiday destination that should be visited once in a lifetime.
-                        While the beautiful Dal and Nigeen lakes in Srinagar offer an opportunity to relish a stay at the houseboat and enjoy a Shikara Ride; the alpine lakes like Tarsar Marsar, Vishnasar, Gadsar Lakes call out the adventure lovers for the best trekking experience.</p>
+                    <Container >
+                        <p>
+                            <span className='txt'>About Kashmir Tourism</span>< br />
+                            "Paradise on the Earth" the state is world famous for its scenic splendour, snow-capped mountains, plentiful wildlife, hospitable people and local handicraft.
+                            With apple orchards, saffron fields, glittering lakes & breathtaking meadows, Kashmir, in the union territory of Jammu & Kashmir, draws the attention of tourists from across the globe. With tranquil settings, magnificent views, and pristine nature, Kashmir is a perfect holiday destination that should be visited once in a lifetime.
+                            While the beautiful Dal and Nigeen lakes in Srinagar offer an opportunity to relish a stay at the houseboat and enjoy a Shikara Ride; the alpine lakes like Tarsar Marsar, Vishnasar, Gadsar Lakes call out the adventure lovers for the best trekking experience.</p>
 
-                    <div className='imgs'>
-                        <img src='https://kashmirtravelagent.com/images/kashmir.png' className='imgg' />
-                    </div>
-                </div>
-                <div className='exp-wrp'>
-                    <button className='exp-btn'>Talk To An Expert</button>
+                        <div className='imgs'>
+                            <img src='https://kashmirtravelagent.com/images/kashmir.png' className='imgg' />
+                        </div>
+
+                        <div className='exp-wrp'>
+                            <button className='exp-btn'>Talk To An Expert</button>
+                        </div>
+                    </Container>
                 </div>
             </section>
+
             <Adventor />
         </>
     )
