179586a17417b63ed3b309e5cc3a0283eb8ffaf5
Your Name <chanrhamni>
2023-03-10 22:45:21 +0530

    update header components add container

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,86 +1,113 @@
+import { Phone } from "phosphor-react";
 import { useForm } from "react-hook-form"
 import TravelDtl from "../travelDtl/TravelDtl";
 import "./Header.css"
+import { Container } from "./Styled";
 const Header = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = () => {
-
+    const onSubmit = (data) => {
+        console.log(data)
     }
 
 
     return (
         <>
-            <div className="conatiner">
-                <div className="title-heading">
-                    <h1 className="txt">Best Travel Agents In Kashmir <span className="txt-primary"> UP TO 70% OFF </span> New Year & Christmas Sale 2023 Offers</h1>
-                    <p className="deals">Book customized Kashmir vacation packages with exciting deals & offers.</p>
-                </div>
+            <main className="header-wrapper">
+                <div className="header-info">
+                    <div className="header-flex">
+                        <Container>
+                            <div className="flex">
+                                <div className="title-heading">
+                                    <h1 className="txt">Best Travel Agents In Kashmir <span className="txt-primary"> UP TO 70% OFF </span> New Year & Christmas Sale 2023 Offers</h1>
+                                    <p className="deals">Book customized Kashmir vacation packages with exciting deals & offers.</p>
 
-                <div className="form-wrapper">
-                    <form onSubmit={handleSubmit(onSubmit)} className="form-conatiner">
+                                    <div className="phn-num">
+                                        <a href="#">
+                                            <Phone size="32" />    : +917006036459
+                                        </a>
+                                    </div>
 
-                        <div className="img-align">
-                            <img src="https://kashmirtravelagent.com/images/kta-logo.png" />
-                        </div>
-                        <input
-                            type="text"
-                            placeholder="enter name"
-                            {...register('name', { required: true })}
-                        />
+                                    <div className="phn-num">
+                                        <a href="#">
+                                            <Phone size="32" />    : +917006036459
+                                        </a>
+                                    </div>
+                                </div>
 
-                        <input
-                            type="email"
-                            placeholder="enter your email"
-                            {...register('email', { required: true })}
-                        />
 
-                        {errors.email && errors.email.type === "required" && <span className="msg">
-                            This is required
-                        </span>}
+                                <div className="form-wrapper">
+                                    <form onSubmit={handleSubmit(onSubmit)} className="form-conatiner">
 
-                        <input
-                            type="number"
-                            placeholder="enter your number"
-                            {...register('number', { required: true })}
-                        />
+                                        <div className="img-align">
+                                            <img src="https://kashmirtravelagent.com/images/kta-logo.png" />
+                                        </div>
+                                        <input
+                                            type="text"
+                                            placeholder="enter name"
+                                            {...register('name', { required: true })}
+                                        />
 
-                        <button type="submit" className="btn">Submit</button>
-                    </form>
-                    <div>
+                                        <input
+                                            type="email"
+                                            placeholder="enter your email"
+                                            {...register('email', { required: true })}
+                                        />
 
+                                        {errors.email && errors.email.type === "required" && <span className="msg">
+                                            This is required
+                                        </span>}
+
+                                        <input
+                                            type="number"
+                                            placeholder="enter your number"
+                                            {...register('number', { required: true })}
+                                        />
+
+                                        <button type="submit" className="btn">Submit</button>
+                                    </form>
+                                    <div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Container>
                     </div>
 
                 </div>
-            </div>
-            <section >
+
                 <div className="info-wrapper">
-                    <div className="containers">
-                        <div className="txt-style">
-                            Best Price Guaranteed
-                        </div>
+                    <Container>
+                        <div className="info-container">
+                            <div className="txt-style">
+                                Best Price Guaranteed
+                            </div>
 
-                        <div className="txt-style">
-                            24/7 Customer Support
-                        </div>
-                        <div className="txt-style">
-                            100% Secure & Covid 19 safety
-                        </div>
+                            <div className="txt-style">
+                                24/7 Customer Support
+                            </div>
+                            <div className="txt-style">
+                                100% Secure & Covid 19 safety
+                            </div>
 
-                        <div className="txt-style">
-                            4.9 Star
-                            Google review
-                        </div>
+                            <div className="txt-style">
+                                4.9 Star
+                                Google review
+                            </div>
 
-                        <div className="txt-style">
-                            10+ Years of
-                            Travel Experience
+                            <div className="txt-style">
+                                10+ Years of
+                                Travel Experience
+                            </div>
                         </div>
-                    </div>
+                    </Container>
+
                 </div>
-            </section>
-            <TravelDtl />
+
+                <TravelDtl />
+            </main>
         </>
     )
 }
