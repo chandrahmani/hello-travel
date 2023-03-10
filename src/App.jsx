7dca586957dc6f4601b6519e43cafea4723a3dad
Your Name <chanrhamni>
2023-03-10 22:47:11 +0530

    app js add react router-dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Adventur from './components/adventur/Adventur';
+import Navbar from './components/core/navbar/Navbar';
 import Header from './components/header/Header'
+import Servic from './components/service/Servic';
+import TravelDtl from './components/travelDtl/TravelDtl';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+
+      <BrowserRouter>
+        <Navbar />
+        <Header />
+        <Routes>
+          <Route path='/' element={<Header />}>
+            <Route path='' element={<Servic />} />
+            <Route path='adventur' element={<Adventur />} />
+            <Route path='travel' element={<TravelDtl />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
