b1a6fb2b6ba4d3d9a1a13d0ea140fcb5130ccc8d
Hidayt Rahman <hidaytrahman@gmail.com>
2023-03-11 16:40:46 +0530

    feat(router): integrations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Adventure from './components/adventure/adventure';
-import Navbar from './components/core/navbar/Navbar';
-import Header from './components/header/Header'
-import Service from './components/service/Service';
-import TravelDetails from './components/travelDetails/TravelDetails';
-
+import "./App.css";
+import Navbar from "./components/core/navbar/Navbar";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-
-      <BrowserRouter>
-        <Navbar />
-        <Header />
-        <Routes>
-          <Route path='/' element={<Service />}>
-            <Route path='adventure' element={<Adventure />} />
-            <Route path='travel' element={<TravelDetails />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Navbar />s{/* <Header /> */}
     </div>
-  )
+  );
 }
-
 
 export default App;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <main>
+      Hi asahs
+      {/* <Outlet /> */}
+      <button className="ct_langmode_btn">asas </button>
+    </main>
+  );
+};
+
+export default Root;
