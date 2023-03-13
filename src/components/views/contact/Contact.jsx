139bf9466140de85a01b6750cfce546088be095a
Chand Rahmani <chandrahmani254@gmail.com>
2023-03-13 16:25:13 +0530

    update input type add fullWidth css  add border-radius

--- src/components/views/contact/Contact.jsx
+++ src/components/views/contact/Contact.jsx
@@ -7,53 +7,68 @@ const Contact = () => {
   return (
     <div className="contact-wrapper">
       <div className="contact-container">
-        <div className="contact-info">
-          <h1>Contact Inforamation</h1>
-          <p>pellentesque tempus. Suspendisse auctor ex id tristique pharetra.</p>
+        <div className="info-container">
+          <div className="contact-info">
+            <h1>Contact Information</h1>
+            <p>pellentesque tempus. Suspendisse auctor ex id tristique pharetra.</p>
+            <div className="number">
+              <h3>
+                <Phone size={25} /> +91 7006036459 / +91 9906755156
+              </h3>
+            </div>
 
-          <div className="number">
-            <p>
-              {' '}
-              <Phone size={25} /> +917006036459/+919906755156
-            </p>
+            <div className="email-wrap">
+              <h3>
+                <Envelope size={32} />
+                Hellotravelsindia@gmail.com
+              </h3>
+            </div>
+
+            <div>
+              <h3>
+                <MapPin size={32} /> Head Office: Nowgam near railway station Srinagar Jammu &
+                Kashmir 190015
+              </h3>
+            </div>
+
+            <div className="icon-logo">
+              <a href="#">
+                <InstagramLogo size={32} />
+              </a>
+              <a href="#">
+                <WhatsappLogo size={32} />
+              </a>
+              <a href="#">
+                <LinkedinLogo size={32} />
+              </a>
+            </div>
           </div>
 
-          <div className="adress">
-            <p>
-              <Envelope size={32} /> Hellotravelsindia@gmail.com
-            </p>
-          </div>
+          <div className="form-wrap">
+            <form className="form">
+              <Input type="text" placeholder="Name" fullWidth className="input" />
+              <Input
+                type="email"
+                placeholder="example@email.com"
+                fullWidth
+                scale="large"
+                className="input"
+              />
+              <Input
+                type="number"
+                placeholder="+91 0000000"
+                fullWidth
+                scale="large"
+                className="input"
+              />
 
-          <div>
-            <p>
-              <MapPin size={32} /> Head Office: Nowgam near railway station Srinagar Jammu &
-              Kashmir190015 Sales Office: 1st Floor, Namgyal Complex, Behind Post Office, main
-              Market, Leh – Ladakh 194101, UT ladakh (INDIA)
-            </p>
+              <div className="btn-wrap">
+                <Button variant="primary" className="button" type="submit">
+                  Send
+                </Button>
+              </div>
+            </form>
           </div>
-          <div className="icon-logo">
-            <a href="#">
-              <InstagramLogo size={32} />
-            </a>
-            <a href="#">
-              <WhatsappLogo size={32} />
-            </a>
-            <a href="#">
-              <LinkedinLogo size={32} />
-            </a>
-          </div>
-        </div>
-
-        <div className="form-wrap">
-          <form className="form">
-            <Input type="text" placeholder="name" scale="large" className="input" />
-            <Input type="mail" placeholder="Gmail" scale="large" className="input" />
-            <Input type="number" placeholder="number" scale="large" className="input" />
-            <Textarea placeholder="Enter your message" scale="large" className="text-area" />
-            <Button size="large" variant="primary" className="button" type="submit">
-              Send
-            </Button>
-          </form>
         </div>
       </div>
     </div>
